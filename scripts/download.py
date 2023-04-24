58489b57316ab61c4323d09ab23069b88dbaaf23
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-04-24 19:00:05 -0400

    update download to use urlretrieve
    Signed-off-by: Vivswan Shah <58091053+Vivswan@users.noreply.github.com>

--- scripts/download.py
+++ scripts/download.py
@@ -10,113 +10,122 @@ Downloads the following:
 
 import os
 import shutil
-import sys
 import zipfile
 from pathlib import Path
+from urllib.request import urlretrieve
 
 import requests
+from tqdm import tqdm
 
 
-def download(url, dirpath):
+class MyProgressBar():
+    def __init__(self):
+        self.pbar = None
+
+    def __call__(self, block_num, block_size, total_size):
+        if not self.pbar:
+            self.pbar = tqdm(total=total_size, unit='iB', unit_scale=True)
+            self.pbar.set_description("Downloading")
+
+        downloaded = block_num * block_size
+        if downloaded < total_size:
+            self.pbar.update(downloaded - self.pbar.n)
+        else:
+            self.pbar.close()
+            self.pbar = None
+
+
+def download(url, dirpath, overwrite=False) -> Path:
     filename = url.split('/')[-1]
-    filepath = os.path.join(dirpath, filename)
-    try:
-        u = requests.get(url, stream=True)
-    except Exception:
-        raise Exception(f"URL {url} failed to open")
+    filepath = Path(dirpath).joinpath(filename)
 
-    if Path(filepath).exists():
+    req = requests.head(url, allow_redirects=True)
+    file_size = int(req.headers["Content-Length"])
+    if filepath.exists() and filepath.stat().st_size == file_size and not overwrite:
+        return filepath
+    else:
+        filepath.unlink(missing_ok=True)
+
+    if filepath.exists():
         return filepath
 
-    try:
-        f = open(filepath, 'wb')
-    except Exception:
-        raise Exception(f"Cannot write {filepath}")
-
-    try:
-        filesize = int(u.headers['Content-Length'])
-    except Exception:
-        raise Exception(f"URL {url} failed to report length")
-
-    print("Downloading: %s Bytes: %s" % (filename, filesize))
-
-    downloaded = 0
-    block_sz = 8192
-    status_width = 70
-    while True:
-        buf = u.raw.read(block_sz)
-        if not buf:
-            print('')
-            break
-        else:
-            print('', end='\r')
-        downloaded += len(buf)
-        f.write(buf)
-        status = (("[%-" + str(status_width + 1) + "s] %3.2f%%") %
-                  ('=' * int(float(downloaded) / filesize * status_width) + '>', downloaded * 100. / filesize))
-        print(status, end='')
-        sys.stdout.flush()
-    f.close()
+    urlretrieve(url, filepath, MyProgressBar())
     return filepath
 
 
-def unzip(filepath):
-    print("Extracting: " + filepath)
-    dirpath = os.path.dirname(filepath)
+def unzip(filepath, remove_zip=True):
+    print(f"Extracting: {filepath}")
+
+    if not filepath.exists():
+        raise FileNotFoundError(f"Could not find file: {filepath}")
+
+    filepath = Path(filepath)
+    dirpath = filepath.parent
+
     with zipfile.ZipFile(filepath) as zf:
+        zip_dir = zf.namelist()[0]
         zf.extractall(dirpath)
-    os.remove(filepath)
+
+    if remove_zip:
+        filepath.unlink(missing_ok=True)
+
+    return Path(dirpath).joinpath(zip_dir)
+
+
+def download_zip(url, dirpath, overwrite=False, remove_zip=True):
+    dirpath = Path(dirpath)
+
+    if dirpath.exists() and not overwrite:
+        print(f'Found {dirpath} - skip')
+        return
+
+    if dirpath.exists() and overwrite:
+        shutil.rmtree(dirpath)
+
+    filepath = download(url, dirpath.parent, overwrite=overwrite)
+    zip_dir = unzip(filepath, remove_zip=remove_zip)
+    shutil.move(zip_dir, dirpath)
 
 
 def download_tagger(dirpath):
-    tagger_dir = 'stanford-tagger'
-    if os.path.exists(os.path.join(dirpath, tagger_dir)):
-        print('Found Stanford POS Tagger - skip')
-        return
     url = 'http://nlp.stanford.edu/software/stanford-postagger-2015-01-29.zip'
-    filepath = download(url, dirpath)
-    with zipfile.ZipFile(filepath) as zf:
-        zip_dir = zf.namelist()[0]
-        zf.extractall(dirpath)
-    os.remove(filepath)
-    os.rename(os.path.join(dirpath, zip_dir), os.path.join(dirpath, tagger_dir))
+    tagger_dir = 'stanford-tagger'
+
+    download_zip(url, Path(dirpath).joinpath(tagger_dir))
 
 
 def download_parser(dirpath):
-    parser_dir = 'stanford-parser'
-    if os.path.exists(os.path.join(dirpath, parser_dir)):
-        print('Found Stanford Parser - skip')
-        return
     url = 'http://nlp.stanford.edu/software/stanford-parser-full-2015-01-29.zip'
-    filepath = download(url, dirpath)
-    with zipfile.ZipFile(filepath) as zf:
-        zip_dir = zf.namelist()[0]
-        zf.extractall(dirpath)
-    os.remove(filepath)
-    os.rename(os.path.join(dirpath, zip_dir), os.path.join(dirpath, parser_dir))
+    tagger_dir = 'stanford-parser'
+
+    download_zip(url, Path(dirpath).joinpath(tagger_dir))
 
 
 def download_wordvecs(dirpath):
-    if os.path.exists(dirpath):
+    url = 'http://www-nlp.stanford.edu/data/glove.840B.300d.zip'
+    dirpath = Path(dirpath)
+
+    if dirpath.exists():
         print('Found Glove vectors - skip')
         return
     else:
-        os.makedirs(dirpath)
-    url = 'http://www-nlp.stanford.edu/data/glove.840B.300d.zip'
+        dirpath.mkdir(parents=True)
+
     unzip(download(url, dirpath))
 
 
 def download_sst(dirpath):
-    if os.path.exists(dirpath):
+    url = 'http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip'
+    dirpath = Path(dirpath)
+
+    if dirpath.exists():
         print('Found SST dataset - skip')
         return
-    url = 'http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip'
-    parent_dir = os.path.dirname(dirpath)
-    unzip(download(url, parent_dir))
-    os.rename(
-        os.path.join(parent_dir, 'stanfordSentimentTreebank'),
-        os.path.join(parent_dir, 'sst'))
-    shutil.rmtree(os.path.join(parent_dir, '__MACOSX'))  # remove extraneous dir
+
+    unzip(download(url, dirpath.parent))
+    # rename dir from 'stanfordSentimentTreebank' to 'sst'
+    dirpath.parent.joinpath('stanfordSentimentTreebank').rename(dirpath.parent.joinpath('sst'))
+    shutil.rmtree(dirpath.parent.joinpath('__MACOSX'))  # remove extraneous dir
 
 
 if __name__ == '__main__':
